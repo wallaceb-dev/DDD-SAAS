8fea5dfeb0b9d3e93e87bf7e1469d15c1babee40
Wallace <wallaceb.teixeira@gmail.com>
2026-03-25 17:50:28 -0300

    fix: :recycle: fix typo in PlanId test

--- tests/Unit/Domain/Subscription/ValueObjects/PlanIdTest.php
+++ tests/Unit/Domain/Subscription/ValueObjects/PlanIdTest.php
@@ -5,5 +5,5 @@ use App\Domain\Subscription\ValueObjects\PlanId;
 it('creates a plan id', function () {
     $planId = PlanId::fromString('plan_1');
 
-    expect($plaId->value())->toBe('plan_1');
+    expect($planId->value())->toBe('plan_1');
 });
