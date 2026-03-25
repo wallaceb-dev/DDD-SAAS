2ac148ac4560d6b2e6937994ee097de58f6690aa
Wallace <wallaceb.teixeira@gmail.com>
2026-03-25 17:42:17 -0300

    refactor: :recycle: refactor naming

--- tests/Unit/Domain/Subscription/ValueObjects/CustomerIdTest.php
+++ tests/Unit/Domain/Subscription/ValueObjects/CustomerIdTest.php
@@ -3,7 +3,7 @@
 use App\Domain\Subscription\ValueObjects\CustomerId;
 
 it('creates a customer id', function () {
-    $id = CustomerId::fromString('cus_1');
+    $CustomerId = CustomerId::fromString('cus_1');
 
-    expect($id->value())->toBe('cus_1');
+    expect($CustomerId->value())->toBe('cus_1');
 });
